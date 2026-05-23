9ae4c266435f725d10ce0c1e405959ebd950a237
Andre <yanuar.andre28@gmail.com>
2026-05-23 14:19:33 +0700

    fix vercel base path

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-// MODE: 'production' = deploy ke domain sendiri (base: '/')
-//       'local'      = localhost/katalog/dist/ (base: '/katalog/dist/')
-const isProduction = process.env.DEPLOY_TARGET === 'production';
-
 export default defineConfig({
   plugins: [react()],
-  base: isProduction ? '/' : '/katalog/dist/',
+  base: '/',
   server: {
     proxy: {
-      '/katalog/dist/api': {
+      '/api': {
         target: 'http://localhost:80',
         changeOrigin: true,
       }
